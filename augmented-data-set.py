c15880eb2d27cd0018454db627209717c58ec7df
Ciro B Rosa <37887021+cirobr@users.noreply.github.com>
2020-10-31 11:40:22 -0300

    definidas funções de transformação

--- augmented-data-set.py
+++ augmented-data-set.py
@@ -10,32 +10,45 @@ from skimage import io
 from skimage.color import rgb2gray
 import numpy as np
 
+# definição de funções de transformação
+def transf_log2(img, c):
+    img2 = c * np.log2(1 + img)
+    return img2
+
+def transf_gama(img, c, gama):
+    img2 = c * np.power(img, gama)
+    return img2
+
+
 #Informar o caminho da pasta que estão as fotos.
 pasta = 'fotos-teste'
 lista_fotos = os.listdir(pasta)
 
+# ler fotos e transformar
 imagem = []
 for foto in lista_fotos:
     # ler a foto
     fullname = pasta + '/' + foto
     imagem.append(io.imread(fullname))
 
-    # mostrar a foto original
+    # foto original
     img = imagem[-1]
     io.imshow(img)
     io.show()
 
-    # mostrar foto cinza
+    # foto cinza
     img_gray = rgb2gray(img)
     io.imshow(img_gray)
     io.show()
 
-    # mostrar foto log10
-    img_log = np.log(img_gray)
-    io.imshow(img_log)
+    # foto log2
+    img_log2 = transf_log2(img_gray, c = 1)
+    io.imshow(img_log2)
     io.show()
 
-    # mostrar foto e (2,718...)
-    img_exp = np.exp(img_gray)
-    io.imshow(img_exp)
+    # foto exponencial
+    img_gama = transf_gama(img_gray, c = 1, gama = 2)
+    io.imshow(img_gama)
     io.show()
+    
+    break
