e568fa813e6200381bfbe10ec04671096bc4afc9
Ciro B Rosa <37887021+cirobr@users.noreply.github.com>
2020-11-10 17:18:23 -0300

    Update ep1-parte2-mnist-like.py

--- ep1-parte2-mnist-like.py
+++ ep1-parte2-mnist-like.py
@@ -1,4 +1,4 @@
-import numpy as np
+#import numpy as np
 from skimage import io
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -16,15 +16,62 @@ metafile = "grade-fotos.csv"
 filename = pasta + metafile
 df = pd.read_csv(filename, sep=";")
 
+# dimensões das fotos
+s = [(3096, 4128, 3), (2608, 4640, 3)]
+
 # lista de classes
 classes = ["garfo", "faca", "colher", "copo", "caneca", "alicate", "chave", "caneta", "livro", "caderno"]
 
 # filtrar um objeto representativo por classe
-seq = "1"
+seq = 1
 tipo = "a"
 fundo = "branco"
 ilum = "indoor dia"
 
-#df2 = df[df.sequencia == seq, df.tipo_obj == tipo, df.fundo == fundo, df.iluminacao == ilum]
-df2 = df[df.sequencia == seq]
-print(df2.head(5), "\n")
+df = df[(df.sequencia == seq) &
+        (df.tipo_obj == tipo) &
+         (df.fundo == fundo) &
+         (df.iluminacao == ilum)]
+#print(df, "\n")
+
+df = df.sort_values(by=["tipo_obj", "objeto"])
+classes.sort()
+#print(df, "\n")
+
+fotos = []
+for ind in df.index:
+    arq = df["arquivo"][ind]
+    arq = fix_jpeg_name(arq)
+    f = pasta + arq
+    #print(df["objeto"][ind], f)
+ 
+    # ler foto
+    img = io.imread(f)
+    """
+    if img.shape not in s:
+        img = np.transpose(img, axes=(1, 0, 2))
+    """
+    fotos.append(img)
+    
+    """
+    # apresentar foto
+    io.imshow(img)
+    io.show()
+    """
+    #break
+
+# plotagem de fotos "mnist-like"
+plt.close("all")
+f, ax = plt.subplots(2, 5, figsize=(10, 10), sharey=(True))
+f.suptitle('Classes de Objetos', fontsize=26)
+
+for i in range(5):
+    ax[0, i].set_title(classes[i])
+    ax[0, i].set(xticks=[], yticks=[])
+    ax[0, i].imshow(fotos[i])
+for i in range(5, 10):
+    ax[1, i-5].set_title(classes[i])
+    ax[1, i-5].set(xticks=[], yticks=[])
+    ax[1, i-5].imshow(fotos[i])
+plt.tight_layout()
+io.show()
