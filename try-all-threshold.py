f6e4f436c4519a980e9c4374e7ad725e4f1c75fa
Ciro B Rosa <37887021+cirobr@users.noreply.github.com>
2020-11-28 17:34:49 -0300

    Create try-all-threshold.py

---
+++ try-all-threshold.py
@@ -0,0 +1,25 @@
+import os
+import numpy as np
+from skimage import io
+from skimage import exposure
+from skimage.filters import try_all_threshold
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# arquivo de entrada em cinza
+#pasta1 = './fotos-teste'
+pasta1 = "./augmentedDataset"
+lista_fotos = os.listdir(pasta1)
+
+i = 0
+for foto in lista_fotos:
+    fullname1 = pasta1 + '/' + foto
+    img = io.imread(fullname1)
+    fig, ax = try_all_threshold(img, figsize=(10, 8), verbose=False)
+    plt.show()
+    """
+    i += 1
+    if i > 10:
+        break
+    """
