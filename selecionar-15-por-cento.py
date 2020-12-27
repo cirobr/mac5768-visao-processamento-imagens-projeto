ed57cbfb60f961c868d49a8fd934479ceb5cd231
cirobr <ciro.rosa@alumni.usp.br>
2020-12-27 10:10:50 -0300

    selecionar fotos v2

--- selecionar-15-por-cento.py
+++ selecionar-15-por-cento.py
@@ -14,18 +14,31 @@ filename1 = pasta1 + metafile1
 df1 = pd.read_csv(filename1, sep=";")
 print(df1.head(5), "\n")
 
-# tamanho da amostra para processamento manual
-sample_size = int(len(df1) * 0.15 / 2)
+# tamanho da amostra por classe para processamento manual
+sample_size = int(len(df1) * 0.15 / 10)
 
 # filtrar metadados
 filtro = (df1.fundo == "branco") & (df1.responsavel == "Ciro")
-df2 = df1[filtro]
+df_temp1 = df1[filtro]
+classes = df_temp1["objeto"].unique()
 
-# sortear amostra aleatoria para processamento manual
-seed = 1
-df2 = df2.sample(n            = sample_size, 
-                 replace      = False, 
-                 random_state = seed)
+# criar data frame vazio
+df2 = pd.DataFrame(data = None, columns = df1.columns)
+
+for classe in classes:
+    # selecionar classe
+    filtro = (df_temp1.objeto == classe)
+    df_temp2 = df_temp1[filtro]
+    
+    # sortear amostra aleatoria para processamento manual
+    seed = 1
+    df_temp2 = df_temp2.sample(n            = sample_size,
+                               replace      = False, 
+                               random_state = seed)
+   
+    # adicionar df_temp2 a df2
+    df2 = df2.append(df_temp2, ignore_index=True)
+
 df2.transformacao = "seg manual"
 
 # copiar fotos para processamento manual
